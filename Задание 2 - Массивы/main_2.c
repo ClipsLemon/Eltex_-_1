8176e4b2e52516b6f1c9d1136f97abb21410f6d0
Sance Pasta <sancepas@student.21-school.ru>
2024-06-24 15:03:38 +0700

    Static array fix

--- Задание 2 - Массивы/main_2.c
+++ Задание 2 - Массивы/main_2.c
@@ -10,32 +10,6 @@ void clear_input_buffer() {
     ;
 }
 
-/**
- * @brief Функция обработки ввода. Нужна, для упрощения обработки ввода.
- * Записывает только целые положительные числа.
- * @param number - адрес на переменную, в которую будет осуществляться запись
- */
-void input_number_handler(int *number) {
-  while (1) {
-    printf("Введите размер массива: ");
-    // если число считалось, то выходим из цилка
-    if (scanf("%d", number)) {
-      if (*number < 1) {
-        printf(
-            "\033[31mВы ввели число меньше 1! Введите число "
-            "повторно.\033[0m\n");
-      } else {
-        break;
-      }
-    } else {
-      printf(
-          "\033[31mСкорее всего, вы ввели строку! Введите число "
-          "повторно.\033[0m\n");
-    }
-    clear_input_buffer();
-  }
-}
-
 void input_array_handler(int *array_element) {
   while (1) {
     // если число считалось, то выходим из цилка
@@ -57,26 +31,31 @@ void input_array_handler(int *array_element) {
  * @param array - константый указатель на массив, чтобы не было возможно его
  * изменить
  */
-void print_array(int size, const int *array) {
-  for (int r = size - 1; r >= 0; r--) {
+void print_array(int size, int *array) {
+  for (int r = 0; r < size; r++) {
     printf("%d ", array[r]);
   }
   printf("\n");
 }
 
 int main() {
-  int n;
-
-  // считываем размерность массив от [1, INF)
-  input_number_handler(&n);
+  int n = 6;
 
   int array[n];
+  printf("Размерность массива = %d\n", n);
   // пишем значения в массив только если матрица не одинарная
   printf("Введите данные массива через пробел: ");
   for (int r = 0; r < n; r++) {
     input_array_handler(array + r);
   }
 
+  for (int r = 0; r < n / 2; r++) {
+    array[r] ^= array[n - r - 1];
+    array[n - r - 1] ^= array[r];
+    array[r] ^= array[n - r - 1];
+  }
+
   print_array(n, array);
+
   return 0;
 }
