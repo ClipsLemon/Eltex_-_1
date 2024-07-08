a41b434c25207b9c1047f7ecb121bf761a88fc68
Sance Pasta <sancepas@student.21-school.ru>
2024-07-08 16:41:15 +0700

    Есть posix & system V

--- Task_13/1/POSIX/client.c
+++ Task_13/1/POSIX/client.c
@@ -1,25 +1,47 @@
+#include <mqueue.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-#define QUEUE_FILENAME "server"
 #define MESSAGE_SIZE 100
-#define SERVER 1
-#define CLIENT 2
-
-struct msgbuf {
-  long mtype;
-  char message_text[MESSAGE_SIZE];
-};
+#define SERVER_PR 1
+#define CLIENT_PR 2
+#define QUEUE_SERVER "/queue_server"
+#define QUEUE_CLIENT "/queue_client"
 
 int main() {
-  key_t queue_key;
-  int queue_id;
-  struct msgbuf message_from_server;
-  struct msgbuf message_to_server;
+  mqd_t mqdes_server;
+  mqd_t mqdes_client;
+  char message[256];
+
+  unsigned int priority = CLIENT_PR;
+
+  struct mq_attr attr;
+  attr.mq_flags = 0;
+  attr.mq_maxmsg = 10;
+  attr.mq_msgsize = 256;
+  attr.mq_curmsgs = 0;
+
+  mqdes_server =
+      mq_open(QUEUE_SERVER, O_CREAT | O_RDWR, S_IWUSR | S_IRUSR, &attr);
+  mqdes_client =
+      mq_open(QUEUE_CLIENT, O_CREAT | O_RDWR, S_IWUSR | S_IRUSR, &attr);
+
+  mq_receive(mqdes_client, message, sizeof(message), &priority);
+
+  printf("Message from server: %s\n", message);
+
+  strncpy(message, "Hello!", 6);
+
+  mq_send(mqdes_server, message, sizeof(message), SERVER_PR);
+
+  mq_close(mqdes_client);
+  mq_close(mqdes_server);
 
   return 0;
 }
