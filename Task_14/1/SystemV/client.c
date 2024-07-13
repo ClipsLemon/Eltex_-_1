c393efe1bd8ce43e635a9ff21779e69c9c1d493f
Sance Pasta <sancepas@student.21-school.ru>
2024-07-13 16:22:23 +0700

    upd

--- Task_14/1/SystemV/client.c
+++ Task_14/1/SystemV/client.c
@@ -5,12 +5,25 @@
 #include <sys/shm.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <stdlib.h>
 
 int main() {
   key_t key = ftok("server.c", 0);
+  if (key == -1) {
+    printf("ERROR: %s", strerror(errno));
+    exit(1);
+  }
   // клиент подключается
   int shm_id = shmget(key, 64, IPC_CREAT);
+  if (shm_id == -1) {
+    printf("ERROR: %s", strerror(errno));
+    exit(1);
+  }
   char *message = (char *)shmat(shm_id, NULL, 0);
+  if (message == (void *)-1) {
+    printf("ERROR: %s", strerror(errno));
+    exit(1);
+  }
 
   printf("%s\n", message);
 
--- Task_14/1/SystemV/server.c
+++ Task_14/1/SystemV/server.c
@@ -1,18 +1,31 @@
 #include <errno.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 int main() {
   key_t key = ftok("server.c", 0);
+  if (key == -1) {
+    printf("ERROR: %s", strerror(errno));
+    exit(1);
+  }
   // создатель может читать и писать и все остальные тоже
   int shm_id =
       shmget(key, 64, IPC_CREAT | S_IWUSR | S_IRUSR | S_IROTH | S_IWOTH);
+  if (shm_id == -1) {
+    printf("ERROR: %s", strerror(errno));
+    exit(1);
+  }
   char *message = (char *)shmat(shm_id, NULL, 0);
+  if (message == (void *)-1) {
+    printf("ERROR: %s", strerror(errno));
+    exit(1);
+  }
   strncpy(message, "Hello client!", 14);
 
   // ждем от пользователя ответного сообщения
